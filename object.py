cecf52c0a803e2d383c618b90cb3d1c72af64120
jake <jacobjenks@gmail.com>
2016-07-18 23:49:18 -0600

    More object detector improvements

--- object.py
+++ object.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
-import rospy
 import json
 
-
-# Shared properties for all objects of a given type
 class Object:
 	'''
 	This class contains the definitions of the objects we are searching for
 	'''
 	classes = [] # Static list of classes we're looking for
 	objects = [] # Static list of objects we're looking for
+	num_classes = 0
 
-	def __init__(self, classID, width, height, color):
+	def __init__(self, classID, objectID, width, height, color):
 		self.classID = classID
+		self.objectID = objectID
 		self.width = width 
 		self.height = height
 		self.color = color
@@ -23,9 +22,13 @@ class Object:
 			data = json.load(data)
 		for c in data["classes"]:
 			Object.classes.append(c)
+	
+		Object.num_classes = len(Object.classes)
 
+		i = 0
 		for obj in data["objects"]:
-			Object.objects.append(Object(Object.classes.index(obj["class"]), obj["width"], obj["height"], obj["color"]))
+			Object.objects.append(Object(Object.classes.index(obj["class"]), i, obj["width"], obj["height"], obj["color"]))
+			i++
 	
 	@staticmethod
 	def getObject(classID, color):
@@ -84,21 +87,6 @@ class ObjectDetection:
 		yMax = self.yMax if yMax == -1 else yMax
 		return self.pixelRatio/self.height/(yMax - yMin)
 
-def initObjects(): 
-	global objects
-	objects.append(Object(2, "red buoy", 0.2, 0.24, Color(255, 0, 0)))
-	objects.append(Object(2, "green buoy", 0.2, 0.24, Color(0, 255, 0)))
-	objects.append(Object(2, "yellow buoy", 0.2, 0.24, Color(255, 255, 0)))
-	objects.append(Object(3, "channel", 2.4, 1.2, Color(255, 255, 0)))
-	objects.append(Object(4, "bin", 0.3, 0.6, Color(255, 255, 0)))
-	objects.append(Object(5, "bin_cover", 0.3, 0.6, Color(0, 0, 0)))
-	objects.append(Object(6, "torpedo target", 1.2, 1.2, Color(255, 255, 0)))
-	objects.append(Object(7, "torpedo hole", 0.3, 0.3, Color(0, 0, 0)))
-	objects.append(Object(8, "torpedo cover", 0.25, 0.25, Color(0, 0, 0)))
-	objects.append(Object(9, "path marker", 1, 0.2, Color(0, 0, 0)))#Fix size
+##########################
 
-if __name__ == '__main__':
-	try:
-		Object.initDefinitions()
-	except rospy.ROSInterruptException:
-		pass
+Object.initDefinitions()
--- object_detector.py
+++ object_detector.py
@@ -19,29 +19,41 @@ from cv_bridge import CvBridge
 from hector_worldmodel_msgs.msg import ImagePercept
 from sensor_msgs.msg import Image
 
+import object
 
-
+class TopicDetect:
+	'''
+	Topic we want to observe for object detections, and the rate to do it at.
+	TODO: Move detection image publishing in here, and make new topic have similar name to old one
+	'''
+	def __init__(self, name, rate):
+		self.name = name
+		self.rate = rospy.Rate(rate)
 
 class ObjectDetector:
+	'''
+	This is a ROS node that keeps a faster rcnn model alive and ready to work on the GPU. 
+	It accepts images from specified topics, and publishes subsequent object detections.
+	'''
 
 	detection_threshold = .05 	# Minimum softmax score
 	pubImagePercept = None		# Publisher for object detection ImagePercept output
 	pubObjectDetector = None	# Publisher for object detection Image output
-	imageSubChannel = None 		# ROS topic we subscribe to for images to classify
-	lastImage = None			# Most recent image message from Image subscription topic
+	imageSubChannels = []		# ROS topics we subscribe to for images to classify
+	imageMsg = None			# Most recent image message from Image subscription topic
 	CvBridge = None				# ROS CVBridge object
 	objectDefinitions = None	# List of Objects
 
 	def __init__(self, gpu_id = 0, cfg_file = "experiments/cfgs/msu.yml", 
 					prototxt = "models/msupool/ZF/faster_rcnn_alt_opt/faster_rcnn_test.pt", 
 					caffemodel = "output/faster_rcnn_alt_opt/msupool/ZF_faster_rcnn_final.caffemodel", 
-					imageSubChannel = "sensors/camF"):
+					imageSubChannels = [TopicDetect("sensors/camF", 60)]):
 		'''
 		@param gpu_id: The ID of the GPU to use for caffe model
 		@param cfg_file: Path to the config file used for the caffe model
 		@param prototxt: Path to network structure definition for caffe model
 		@param caffemodel: Path to caffemodel containing trained network weights
-		@param imageSubChannel: ROS topic we subscribe to for images to classify
+		@param imageSubChannels: ROS topics we subscribe to for images to classify
 		'''
 		global cfg
 
@@ -56,63 +68,65 @@ class ObjectDetector:
 		self.net.name = os.path.splitext(os.path.basename(caffemodel))[0]
 
 		#Initialize ROS
-		pubImagePercept = rospy.Publisher('worldmodel/image_percept', ImagePercept, queue_size=10)
-		pubObjectDetector = rospy.Publisher('object_detector', Image, queue_size=10)
-		rospy.Subscriber(imageSubChannel, Image, self.subImageCB)
+		self.pubImagePercept = rospy.Publisher('worldmodel/image_percept', ImagePercept, queue_size=10)
+		self.pubObjectDetector = rospy.Publisher('object_detector', Image, queue_size=10)
+		self.imageSubChannels = imageSubChannels
+		for sub in self.imageSubChannels:
+			rospy.Subscriber(sub.name, Image, self.subImageCB)
 		rospy.init_node("object_detector")
 
 		self.CvBridge = CvBridge()
 
-		self.lastImage = self.CvBridge.cv2_to_imgmsg(cv2.imread("data/MSUPool/Images/GOPR0424.JPG"))
-		
 		rate = rospy.Rate(10)
 		while not rospy.is_shutdown():
-			objects = self.detect(self.CvBridge.imgmsg_to_cv2(self.lastImage))
-			print objects
-			self.publishDetections(objects)
+			if self.imageMsg is not None:
+				image = self.CvBridge.imgmsg_to_cv2(self.imageMsg)
+				objects = self.detect(image)
+				self.publishDetections(objects)
+				self.imageMsg = None
 			rate.sleep()
 
 	def subImageCB(self, image):
-		self.lastImage = image
-
+		#TODO: multiple image topics		
+		if self.imageMsg is None:
+			self.imageMsg = image
 
 	def publishDetections(self, objects):
 		for o in objects:
 			#ImagePercept
 			msgImagePercept = ImagePercept()
-			msgImagePercept.header = self.lastImage.header
+			msgImagePercept.header = self.imageMsg.header
 			msgImagePercept.camera_info = None #TODO
-			msgImagePercept.x = (o.rect.xMin + o.rect.xMax) / 2 #Center point of object
-			msgImagePercept.y = (o.rect.yMin + o.rect.yMax) / 2 
-			msgImagePercept.width = (o.rect.xMax - o.rect.xMin) / msgImagePercept.camera_info.width
-			msgImagePercept.height = (o.rect.yMax - o.rect.yMin) / msgImagePercept.camera_info.height
+			msgImagePercept.info.class_id = o.classID
+			msgImagePercept.info.object_id = o.objectID
+			msgImagePercept.info.name = o.name()
+			msgImagePercept.x = (o.xMin + o.xMax) / 2 #Center point of object
+			msgImagePercept.y = (o.yMin + o.yMax) / 2 
+			msgImagePercept.width = (o.xMax - o.xMin) / msgImagePercept.camera_info.width
+			msgImagePercept.height = (o.yMax - o.yMin) / msgImagePercept.camera_info.height
 			msgImagePercept.distance = o.distance()
 			self.pubImagePercept.publish(msgImagePercept)
 
 			#Image
-			image = self.lastImage.clone()
+			image = self.CvBridge.bridge.imgmsg_to_cv2(self.imageMsg)
 
 			for o in objects:
-				cv2.rectangle(image, (o.rect.xMin, o.rect.yMin), (o.rect.xMax, o.rect.yMax), (0, 255, 0))
+				cv2.rectangle(image, (o.xMin, o.yMin), (o.xMax, o.yMax), (0, 255, 0))
 				cv2.putText(image, o.name() + ":" + o.distance(), FONT_HERSHEY_SIMPLEX, 1, (0,255,0)) 
 
 			self.pubObjectDetector.publish(self.CvBridge.bridge.cv2_to_imgmsg(image))
 
 	# Wrapper for faster-rcnn detections
 	# Excluded max_per_image - look into it if it becomes a problem
-	# TODO:
-	# 	num classes - actually load imdb?
-	#	merge with existing ROS thing
-	#	store parameters in main function somewhere
 	def detect(self, image):
 		'''
-		@param image: cv2 image for detecting objects
+		@param image: cv2 image to detect objects in
 		'''
 		scores, boxes = im_detect(self.net, image)
 
 		objects = []
 
-		for j in xrange(1, 11):
+		for j in xrange(1, Object.num_classes):
 			inds = np.where(scores[:, j] > self.detection_threshold)[0]
 			cls_scores = scores[inds, j]
 			cls_boxes = boxes[inds, j*4:(j+1)*4]
@@ -131,13 +145,12 @@ class ObjectDetector:
 
 	def avgColor(self, image, x, y, width, height):
 		'''
-		Get average color of middle subset% of object
+		Get average color of middle crop of object
 		'''
-		subset = .5
-		xSub =  (width * subset)/2
-		ySub = (height * subset)/2
+		crop = .5
+		xSub =  (width * crop)/2
+		ySub = (height * crop)/2
 		return cv2.mean(image[x+xSub:y+ySub, width-xSub:height-ySub])
 
 if __name__ == '__main__':
 	detector = ObjectDetector()
-	
